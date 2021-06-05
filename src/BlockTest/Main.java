306d97ccbc56ef2c84d2ee71bb08c2bcdf8df8f6
Александр Целинко <C:\Users\Home\AppData\Roaming\The Bat!>
2021-06-05 10:05:59 +0300

    Initial commit

---
+++ src/BlockTest/Main.java
@@ -0,0 +1,15 @@
+package BlockTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+                int x,y;
+        y=20;
+                for(x=0;x<10;x++)
+        {
+            System.out.println("Значение х "+x);
+            System.out.println("Значение у "+y);
+            y=y-2;} 
+    }
+}
